5bca9b1ec2a21ce182a96825197abfbd93216bba
farooqui ziauddin <f.ziyauddin@gmail.com>
2022-11-02 16:21:02 +0530

    prime code fixed

--- prime.js
+++ prime.js
@@ -1,15 +1,25 @@
 function checkPrime(num){
+counter=0;
 
 for(i=i;i<=num;i++){
 
-if(num%i){
+if(num%i==0){
 
+counter++
+
+}
+
+}
+
+if(countr==2){
 return true;
-
 }
-
+return false;
 }
-return false
+   ans= checkPrime(13);
 
+if(ans=true){
+console.log("Number is Prime")
+}else{
+console.log("Number is Not Prime")
 }
-checkPrime(13);
